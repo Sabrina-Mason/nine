2f3cbcd7bde47e6a1d32fe73edca34c6f3fb9eb7
Sabrina Mason <fireflyy85@gmail.com>
2017-07-09 22:24:43 -0400

    first commit

---
+++ nine/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace nine
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            public circle (double radius)
+            public double GetCircumference()
+            public String GetFormattedCircumference()
+            public double GeatArea()
+            public String GetFormattedArea()
+            private String FormatNumber(double x)
+
+                const NetPipeStyleUriParser = System.Math class.
+        //we kind of went over this a little bit in class
+
+        }
+}
+}
